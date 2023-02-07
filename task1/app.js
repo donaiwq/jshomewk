2eb50319df251225aa2f56eb2ada279b6ac98b4a
donaiwq <a_uzakbaeva@bk.ru>
2023-02-07 18:13:49 +0300

    new task

--- task1/app.js
+++ task1/app.js
@@ -1,14 +1,31 @@
-var getNum = Number(prompt("Please enter your rate"));
-if (getNum ===5  ) {
-  alert('Спасибо за высокую оценку!')
-}
-else if (getNum >=10) {
-  alert('Please enter the number from 1 to 5')
 
-}
-else if (getNum<=4) {
-   alert('Мы работаем над улучшением сервиса!')
-}
-else {
- alert('Check your entered number')
+// 1) написать программу: нужно запрашивать у пользователя его имя и фамилию (отдельным prompt) и выводит приветствие в виде: Здравствуйте, Имя Фамилия! (задание на конкатенацию)
+
+var getUserName = prompt("Please enter your name ");
+var getUserSurname = prompt("Please enter your surname");
+var getUserInfo = getUserName + " " + getUserSurname ;
+  alert('Здраствуйте,'+ ' ' + getUserInfo)
+
+// 2) написать программу "светофор": программа запрашивает у пользователя цвет в текстовом виде, и печатает в консоль действие согласно ПДД. Например: красный : стой! желтый : жди и т.д. (задание на switch..case)
+
+var trafficColor = 'Red'.toLocaleLowerCase()
+//  var pddRules = trafficColor ``
+  switch (trafficColor) {
+    case "red":
+      console.log('действие согласно ПДД:'+ " " ,'Stop!');
+      break;
+    case "yellow":
+      console.log('действие согласно ПДД:' + " ",  'Wait your turn' );
+   
+      break;
+    
+     
+    case "green":
+      console.log('действие согласно ПДД:'+ " ", 'Now, its your turn');
+
+      break;
+    
+    default:
+      'Be carefull! '
+      break;
 }
