c544b69a1f4d4ec06b35516328c3c3c9a7da6dea
donaiwq <a_uzakbaeva@bk.ru>
2023-02-08 17:31:28 +0300

    new task

--- task1/app.js
+++ task1/app.js
@@ -29,3 +29,42 @@ var trafficColor = 'Red'.toLocaleLowerCase()
       'Be carefull! '
       break;
 }
+// 3) написать программу "конвертер чисел". Пользователь вводит число от 1 до 9, программа должна сконвертировать это число в римское. Например: 2 -> II, 5 -> V и т.д. (задание на switch..case)
+
+var rimNumber = '10'
+switch (rimNumber) {
+  case '1':
+    console.log('I');
+    break;
+  case '2':
+    console.log("II");
+    break;
+  case '3':
+    console.log("III");
+    break;
+  case '4':
+    console.log('IV');
+    break;
+  case '5':
+    console.log('V');
+    break;
+  case '6':
+    console.log('VI');
+    break;
+  case '7':
+    console.log('VII');
+    break;
+  case '8':
+    console.log('VIII');
+    break;
+  case '9':
+    console.log('IX');
+    break;
+  case '10':
+    console.log('X');
+    break;
+
+  default:
+    console.log('Check your number');
+    break;
+}
